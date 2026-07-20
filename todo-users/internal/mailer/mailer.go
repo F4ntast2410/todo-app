b31f659143cea97a5b3344522f0e28591a88bf43
F4ntast2410 <f4ntast2410@gmail.com>
2026-07-20 16:42:10 +0300

    New features: fixed few secret leaks

--- todo-users/internal/mailer/mailer.go
+++ todo-users/internal/mailer/mailer.go
@@ -1,11 +1,8 @@
 package mailer
 
 import (
-	"crypto/tls"
 	"fmt"
-	"net"
 	"net/smtp"
-	"time"
 )
 
 type Config struct {
@@ -37,52 +34,6 @@ func (m *SMTPMailer) Send2FACode(to string, code string) error {
 	))
 
 	addr := fmt.Sprintf("%s:%s", m.cfg.Host, m.cfg.Port)
-
-	// Если используем порт 465 (Implicit TLS)
-	if m.cfg.Port == "465" {
-		tlsconfig := &tls.Config{
-			ServerName: m.cfg.Host,
-		}
-
-		// 🛠️ Используем DialWithDialer, принудительно задаем "tcp4" и таймаут 10 секунд
-		dialer := &net.Dialer{
-			Timeout: 10 * time.Second,
-		}
-
-		conn, err := tls.DialWithDialer(dialer, "tcp4", addr, tlsconfig)
-		if err != nil {
-			return fmt.Errorf("tls dial failed: %w", err)
-		}
-		defer conn.Close()
-
-		// ... остальной код отправки без изменений
-		client, err := smtp.NewClient(conn, m.cfg.Host)
-		if err != nil {
-			return fmt.Errorf("smtp client creation failed: %w", err)
-		}
-		defer client.Quit()
-
-		auth := smtp.PlainAuth("", m.cfg.Username, m.cfg.Password, m.cfg.Host)
-		if err = client.Auth(auth); err != nil {
-			return fmt.Errorf("auth failed: %w", err)
-		}
-
-		if err = client.Mail(m.cfg.From); err != nil {
-			return err
-		}
-		if err = client.Rcpt(to); err != nil {
-			return err
-		}
-
-		w, err := client.Data()
-		if err != nil {
-			return err
-		}
-		if _, err = w.Write(msg); err != nil {
-			return err
-		}
-		return w.Close()
-	}
 
 	// Для порта 587 (STARTTLS)
 	auth := smtp.PlainAuth("", m.cfg.Username, m.cfg.Password, m.cfg.Host)
