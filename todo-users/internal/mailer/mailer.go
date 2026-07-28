67ce1b9aca2c6c2cd37a6b9894f0df5b6d7cec21
F4ntast2410 <f4ntast2410@gmail.com>
2026-07-28 19:04:01 +0300

    -

--- todo-users/internal/mailer/mailer.go
+++ todo-users/internal/mailer/mailer.go
@@ -1,6 +1,7 @@
 package mailer
 
 import (
+	"crypto/tls"
 	"fmt"
 	"mime"
 	"net/smtp"
@@ -25,7 +26,7 @@ func New(cfg Config) *SMTPMailer {
 func (m *SMTPMailer) Send2FACode(to string, code string) error {
 	subject := mime.BEncoding.Encode("UTF-8", "Код подтверждения")
 	body := fmt.Sprintf(
-		"Ваш код подтверждения: %s\r\n\r\nКод действителен 5 минут. Если вы не запрашивали вход — просто игнорируйте это письмо.",
+		"Ваш код подтверждения: %s\r\n\r\nКод действителен 5 минут.",
 		code,
 	)
 
@@ -36,7 +37,40 @@ func (m *SMTPMailer) Send2FACode(to string, code string) error {
 
 	addr := fmt.Sprintf("%s:%s", m.cfg.Host, m.cfg.Port)
 
-	// Для порта 587 (STARTTLS)
+	// Для порта 465 используем tls.Dial вместо smtp.SendMail
+	conn, err := tls.Dial("tcp", addr, &tls.Config{
+		ServerName: m.cfg.Host,
+	})
+	if err != nil {
+		return fmt.Errorf("tls dial error: %w", err)
+	}
+	defer conn.Close()
+
+	client, err := smtp.NewClient(conn, m.cfg.Host)
+	if err != nil {
+		return fmt.Errorf("smtp client error: %w", err)
+	}
+	defer client.Quit()
+
 	auth := smtp.PlainAuth("", m.cfg.Username, m.cfg.Password, m.cfg.Host)
-	return smtp.SendMail(addr, auth, m.cfg.From, []string{to}, msg)
+	if err := client.Auth(auth); err != nil {
+		return fmt.Errorf("auth error: %w", err)
+	}
+
+	if err := client.Mail(m.cfg.From); err != nil {
+		return err
+	}
+	if err := client.Rcpt(to); err != nil {
+		return err
+	}
+
+	w, err := client.Data()
+	if err != nil {
+		return err
+	}
+	_, err = w.Write(msg)
+	if err != nil {
+		return err
+	}
+	return w.Close()
 }
