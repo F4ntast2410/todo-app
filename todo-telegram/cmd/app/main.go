c61b4d4741a00ce43e2634fed7e3e60c6c675a77
F4ntast2410 <f4ntast2410@gmail.com>
2026-07-30 14:05:43 +0300

    -

--- todo-telegram/cmd/app/main.go
+++ todo-telegram/cmd/app/main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"context"
 	"log/slog"
-	"net"
-	"net/http"
 	"os"
 	"os/signal"
 	"proj/internal/client/taskclient"
@@ -20,14 +17,6 @@ func main() {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 	slog.SetDefault(logger)
 
-	// 🟢 ПРИНУДИТЕЛЬНО ОТПРАВЛЯЕМ ВСЕ HTTP-ЗАПРОСЫ ПО IPv6 (tcp6)
-	http.DefaultTransport = &http.Transport{
-		DialContext: func(ctx context.Context, network, addr string) (net.Conn, error) {
-			var d net.Dialer
-			return d.DialContext(ctx, "tcp6", addr)
-		},
-	}
-
 	cfg := config.MustLoad()
 
 	taskClient, err := taskclient.New(cfg.TaskServiceGRPCAddr)
@@ -36,14 +25,12 @@ func main() {
 		panic(err)
 	}
 	defer taskClient.Close()
-
 	userTgClient, err := usertgclient.New(cfg.UserServiceGRPCAddr)
 	if err != nil {
 		logger.Error("failed to connect to usertg service", slog.String("error", err.Error()))
 		panic(err)
 	}
 	defer userTgClient.Close()
-
 	botServer, err := tgbot.NewBotServer(cfg.BotToken, taskClient, userTgClient, logger)
 	if err != nil {
 		logger.Error("failed to initialize bot", slog.String("err", err.Error()))
